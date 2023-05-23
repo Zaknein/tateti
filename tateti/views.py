0c0fff903599c4d3858b39fb4cc67f157ab15858
German Kröger <gkroger11@gmail.com>
2023-05-23 17:57:45 -0300

    codigo torcido

--- tateti/views.py
+++ tateti/views.py
@@ -4,30 +4,47 @@ mapa={}
 def game(request):
     casilla = request.GET.get("casilla")
 
-    # if casilla == None:
-    if casilla in mapa:
-        return render(request,'game.html', {"caca":mapa})
 
-        
+    if casilla in mapa:
+        return render(request,'game.html', {"valooor":mapa})
+
     mapa[casilla]=""
 
-    print(len(mapa)) 
+
     var = (len(mapa))
     
     if var%2==0:
-        # print(var)
+
         mapa[casilla]="x"
     else:
-        # print(print())
+
         mapa[casilla]="O"    
 
-    # print(casilla)
-    print(mapa)
-     
-    # request.session[casilla] = "O"
+    valooor = mapa[casilla]
 
+    if   (mapa.get("1")=="x"and mapa.get("2")=="x" and mapa.get("3")=="x")|(mapa.get("1") == "O" and mapa.get("2") == "O" and mapa.get("3") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+    elif (mapa.get("4")=="x"and mapa.get("5")=="x" and mapa.get("6")=="x")|(mapa.get("4") == "O" and mapa.get("5") == "O" and mapa.get("6") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
 
-    return render(request,'game.html', {"caca":mapa})
+    elif (mapa.get("7")=="x"and mapa.get("8")=="x" and mapa.get("9")=="x")|(mapa.get("7") == "O" and mapa.get("8") == "O" and mapa.get("9") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+
+    elif (mapa.get("1")=="x"and mapa.get("4")=="x" and mapa.get("7")=="x")|(mapa.get("1") == "O" and mapa.get("4") == "O" and mapa.get("7") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+
+    elif (mapa.get("2")=="x"and mapa.get("5")=="x" and mapa.get("8")=="x")|(mapa.get("2") == "O" and mapa.get("5") == "O" and mapa.get("8") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+
+    elif (mapa.get("3")=="x"and mapa.get("6")=="x" and mapa.get("9")=="x")|(mapa.get("3") == "O" and mapa.get("6") == "O" and mapa.get("9") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+
+    elif (mapa.get("1")=="x"and mapa.get("5")=="x" and mapa.get("9")=="x")|(mapa.get("1") == "O" and mapa.get("5") == "O" and mapa.get("9") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+    elif (mapa.get("3")=="x"and mapa.get("5")=="x" and mapa.get("7")=="x")|(mapa.get("3") == "O" and mapa.get("5") == "O" and mapa.get("7") == "O"):
+        return render(request,'ganaste.html',{"valooor":valooor})
+
+    return render(request,'game.html', {"valooor":mapa})
 
 def reset(request):
     mapa.clear()
