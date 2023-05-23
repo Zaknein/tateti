315e9019fcdff31ada86bfa445ac6ec462d9c425
German Kröger <gkroger11@gmail.com>
2023-05-23 15:12:01 -0300

    basic function

--- tateti/views.py
+++ tateti/views.py
@@ -1,15 +1,34 @@
 from  django.shortcuts import render
 
-
+mapa={}
 def game(request):
     casilla = request.GET.get("casilla")
 
+    # if casilla == None:
+    if casilla in mapa:
+        return render(request,'game.html', {"caca":mapa})
+
+        
+    mapa[casilla]=""
+
+    print(len(mapa)) 
+    var = (len(mapa))
     
-    request.session[casilla] = "O"
+    if var%2==0:
+        # print(var)
+        mapa[casilla]="x"
+    else:
+        # print(print())
+        mapa[casilla]="O"    
+
+    # print(casilla)
+    print(mapa)
+     
+    # request.session[casilla] = "O"
 
 
-    return render(request,'game.html', {"caca":request.session})
+    return render(request,'game.html', {"caca":mapa})
 
 def reset(request):
-    request.session.clear()
+    mapa.clear()
     return render(request,'game.html', {})
